885e6c273bdc7e03ac9e46517a2582a5c4e45ab7
Shunichi Miyashita <miyash.shuni.323sat@gmail.com>
2017-07-28 17:21:02 +0900

    すまーとぼーる(森林)

---
+++ SmartBall/Assets/ball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+    public float ballSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, ballSpeed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SmartBall/Assets/ball_controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_controller : MonoBehaviour {
+
+    public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 placePosition = new Vector3(4.5f, -3f, -6f);
+
+        if (Input.GetButtonDown("Fire1")){
+            Instantiate(ballPrefab, placePosition, Quaternion.identity);
+        }
+
+    }
+}
