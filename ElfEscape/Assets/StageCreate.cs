92b7dec42fe4fec5848498498ea2798120e76acc
Shunichi Miyashita <miyash.shuni.323sat@gmail.com>
2017-08-01 17:34:33 +0900

    男の子が歩く

---
+++ ElfEscape/Assets/StageCreate.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageCreate : MonoBehaviour {
+
+    public GameObject wallPrefab;
+    int wallX = 9;
+    int wallZ = 9;
+
+	// Use this for initialization
+	void Start () {
+        CreateWall();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CreateWall()
+    {
+        //ループでwallXまで壁を生成する
+        for(float f = -0.5f; f <= wallX; f++)
+        {
+            Instantiate(wallPrefab, new Vector3(f + 1, 0, -0.6f), Quaternion.identity);
+            Instantiate(wallPrefab, new Vector3(f + 1, 0, wallX + 1.5f), Quaternion.identity);
+        }
+
+        for (float f = -0.5f; f <= wallZ; f++)
+        {
+            Instantiate(wallPrefab, new Vector3(-0.6f, 0, f + 1), Quaternion.identity);
+            Instantiate(wallPrefab, new Vector3(wallZ + 1.5f, 0, f + 1), Quaternion.identity);
+        }
+    }
+}
---
+++ ElfEscape/Assets/playercontroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+
+    Vector3 moveDirection = Vector3.zero; //移動方向(この関数内でのみ使用)
+    public float speedZ;
+    CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+        cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //前進ベロシティの設定
+        moveDirection.z =  speedZ;
+
+        //移動実行
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);
+        //deltatime→FPSの違いによるキャラクターの移動距離の違いを吸収する
+        cc.Move(globalDirection * Time.deltaTime);
+    }
+}
